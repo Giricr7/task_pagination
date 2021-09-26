baba8e10cfbd930b4c5130327eee3eb9f2adbaae
Giriprasath <giriarjun1995@gmail.com>
2021-09-26 16:34:46 -0600

    added data

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+setdata=  (page)=>{
+    
+
+// fetching json data from given url
+    let url = 'https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json'
+
+    fetch(url)
+    .then((data) => {
+        return data.json();
+    })
+        .then((jsondata) => {
+        
+         return (jsondata);
+        })
+        .then((jsdata) => {
+          
+           
+
+            for (let i = 0; i < jsdata.length; i++){
+
+                table = document.createElement('table');
+                
+                row_1 = table.insertRow();
+                id_row = row_1.insertCell();
+                id_row.textContent = 'Id: ';
+                id_row.setAttribute('id','number')
+                id_data = row_1.insertCell();
+                id_data.textContent = jsdata[i].id;
+
+                row_2 = table.insertRow();
+                name_row = row_2.insertCell();
+                name_row.textContent = 'Name: ';
+                name_row.setAttribute('id','name')
+                name_data = row_2.insertCell();
+                name_data.textContent = jsdata[i].name;
+                
+
+                row_3 = table.insertRow();
+                email_row = row_3.insertCell();
+                email_row.textContent = 'Email: ';
+                email_row.setAttribute('id','email')
+                email_data = row_3.insertCell();
+                email_data.textContent = jsdata[i].email;
+
+                data_div = document.getElementById('display_data');
+                data_div.appendChild(table);
+
+                
+            }
+
+
+ 
+    })
+
+
+
+
+
+}
+
+
+
+
 //creating pagination outlook
 
 let pag_table = document.createElement('table');
@@ -48,7 +111,8 @@ let pag_div = document.getElementById('pag_div')
 
 pag_div.appendChild(pag_table)
 
+//writing eventlistener for all pagination buttons
 pag_numbers = pag_table.getElementsByClassName('paging_buttons');
 
 
-
+setdata();
